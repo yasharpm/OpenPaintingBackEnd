090ad9ae074d887ecf07493e59a632563bee19c4
Yashar <yashar.p@780.ir>
2021-07-19 03:52:54 +0430

    Improved performance

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ application = app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
 
-world = {'image': 'f' * (6 * 256 * 256)}
+world = {'image': ['ffffff'] * (256 * 256)}
 
 @sio.event
 def connect(sid, environ):
     print('connect ', sid)
-    sio.emit('snapshot', world['image'], room=sid)
+    sio.emit('snapshot', ''.join(world['image']), room=sid)
 
 @sio.event
 def paint(sid, data):
@@ -23,9 +23,9 @@ def paint(sid, data):
 
     for touch in touches:
         c = touch['c']
-        i = (touch['y'] * 256 + touch['x']) * 6
+        i = touch['y'] * 256 + touch['x']
 
-        world['image'] = world['image'][:i] + c + world['image'][i + 6:]
+        world['image'][i] = c
 
 
 @sio.event
